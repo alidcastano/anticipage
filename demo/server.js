43acb427c5e38f431a7290c295c4b1926466d405
Joseph Raso <joeraso24@gmail.com>
2017-04-16 14:03:25 -0400

    linting

--- demo/server.js
+++ demo/server.js
@@ -5,28 +5,22 @@ var resolve = require('path').resolve,
     webpackHotMiddleware = require('webpack-hot-middleware'),
     config = require('../webpack.config'),
     fs = require('fs'),
-    MarkovChain = require('markovchain'),
     port = process.env.PORT || 8080,
     projectRoot = resolve(__dirname, '..'),
-    anticipage = require('../src/index');
+    anticipage = require('../src/index').anticipage;
 
 const compiler = webpack(config),
       app = express(),
       router = express.Router()
 
-router.use(anticipage);
 
 router.get('/', function (req, res, next) {
   console.log('ID:', req.params.id);
   next();
 });
 
-var top = 0
-var settings = 0
-var projects = 0
-var total = 0
-
-app.use(router)
+app.use(anticipage);
+app.use(router);
 app.use(webpackHotMiddleware(compiler))
 app.use(webpackDevMiddleware(compiler, {
   hot: true,
--- src/index.js
+++ src/index.js
@@ -1,23 +1,30 @@
-var MarkovChain = require('markovchain');
+var MarkovChain = require('markovchain'),
+  fs = require('fs')
+
+var total = 0, top = 0, settings = 0, projects = 0
 
 function anticipage(req, res, next) {
-    var quotes = new MarkovChain(fs.readFileSync('./user1.txt', 'utf8'));
-    nextRoute  = quotes.start(req.url).end(1).process().split(' ')[1];
-    if (nextRoute === '/top') {
-        top++;
-    }
-    else if (nextRoute === '/settings') {
-        settings++;
-    }
-    else if (nextRoute === '/projects') {
-        projects++;
-    }
-    total++;
-    console.log('settings: ' + settings/total);
-    console.log('projects: ' + projects/total);
-    console.log('top: ' + top/total);
-    res.send('success');
-    next();
+  if (req.path === "/favicon.ico") {
+      return next()
+  }
+
+  var quotes = new MarkovChain(fs.readFileSync('./user1.txt', 'utf8'))
+  var nextRoute = quotes.start(req.url).end(1).process().split(' ')[1]
+  if (nextRoute === '/top') {
+    top++
+  }
+  else if (nextRoute === '/settings') {
+    settings++
+  }
+  else if (nextRoute === '/projects') {
+    projects++
+  }
+  total++
+  console.log('settings: ' + settings/total)
+  console.log('projects: ' + projects/total)
+  console.log('top: ' + top/total)
+  res.send('success')
+  next()
 }
 
 module.exports = {
