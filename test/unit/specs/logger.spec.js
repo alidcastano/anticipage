d1f5e6fbadbefdff08cce4ee07179297ada098e3
Alid Castano <alidcastano@gmail.com>
2017-04-17 19:18:29 -0400

    getting karma to work

---
+++ test/unit/specs/logger.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-expressions */
+/* global describe, it, expect */
+
+describe('Logger', function () {
+  it('is joey a dummy', () => {
+    const answer = true
+    expect(answer).to.be.true
+  })
+})
